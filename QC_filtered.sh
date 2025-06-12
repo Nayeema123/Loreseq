c910f4c9c68f3860efdc81aa150611f6252db756
Nayeema N <nayeemaniz786@gmail.com>
2025-06-12 12:19:33 +0400

    cheching filtered quality - QC_filtered.sh

---
+++ QC_filtered.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Usage: ./QC_filtered.sh
+
+SAMPLE="SRR23082033"
+RESULTDIR="/home/nayeema/LRS2/results"
+
+echo "Running NanoPlot for filtered reads..."
+NanoPlot --fastq ${RESULTDIR}/${SAMPLE}_filtered.fastq --outdir ${RESULTDIR}/filtered_quality
+echo "NanoPlot for filtered reads completed."
