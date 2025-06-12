8a6b876f695b5be4839c6ecc32f2f06a8bc428d5
Nayeema N <nayeemaniz786@gmail.com>
2025-06-12 13:04:54 +0400

    6. Variant calling - cuteSV.sh

--- cuteSV.sh
+++ cuteSV.sh
@@ -3,9 +3,9 @@
 # Usage: ./cuteSV.sh
 
 SAMPLE="SRR23082033"
-ALIGNDIR="/home/nayeema/LRS2/aligned_reads"
+ALIGNDIR="/home/nayeema/LRS/aligned_reads"
 REF="/home/nayeema/LRS/hg38/hg38.fa"
-OUTDIR="/home/nayeema/LRS2/variants"
+OUTDIR="/home/nayeema/LRS/variants"
 
 #This directory is used by CuteSV to store intermediate files during variant calling.
 mkdir -p ${OUTDIR}/cutesv_workdir
